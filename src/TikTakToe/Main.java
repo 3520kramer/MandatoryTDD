a2d41ff5956d154f6b6b864b03a3af7983fde997
Oliver Kramer <3520_kramer@live.dk>
2021-04-04 10:47:06 +0200

    Moved runGame into Main

--- src/TikTakToe/Main.java
+++ src/TikTakToe/Main.java
@@ -3,8 +3,38 @@ package TikTakToe;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
-        game.runGame();
 
+        game.promptUsersForPlayerName();
+
+        game.assignPieces();
+
+        boolean isFinished = false;
+        int rounds = 0;
+        int played = 1;
+
+        while (!isFinished) {
+            rounds++;
+            game.placePieceOnBoard();
+
+            isFinished = game.hasWon(rounds);
+
+            if (rounds == 6 && !isFinished) {
+                game.getBoard().clear();
+                rounds = 0;
+                played++;
+                System.out.println("Resetting game");
+            }
+        }
+
+        String winner; // = rounds == 5 ? player1.getName() : player2.getName();
+
+        if (rounds == 5) { // X wins
+            winner = game.getPlayer1().isX() ? game.getPlayer1().getName() : game.getPlayer2().getName();
+        } else { // O wins
+            winner = !game.getPlayer1().isX() ? game.getPlayer1().getName() : game.getPlayer2().getName();
+        }
+
+        System.out.println("The winner is: " + winner + " in " + played + " rounds");
     }
 }
 
